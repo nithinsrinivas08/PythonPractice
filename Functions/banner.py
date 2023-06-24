bb1aa772820156b6e5e09758927d975b751662d0
Nithin Srinivas <nithinackon@gmail.com>
2023-06-24 16:47:52 +0200

    Banner : set and replace the screenWidth to preferred value

--- Functions/banner.py
+++ Functions/banner.py
@@ -1,7 +1,7 @@
 def bannerText(text, screenWidth = 80) :
     if len(text) > screenWidth - 4 :
         print("EEK!!")
-        print("The text is too long to fit in the specified width")
+        print("The text is too long to fit in the specified width", 60)
     if text == "*" :
         print("*" * screenWidth)
     else :
@@ -9,14 +9,14 @@ def bannerText(text, screenWidth = 80) :
         outputString = "**{}**".format(centeredText)
         print(outputString)
 
-bannerText("*")
-bannerText("Always look on the bright side of life...")
-bannerText("If life seems jolly rotten")
-bannerText("There's something you've forgotten!")
-bannerText("And that's to laugh and smile and dance and sing,")
-bannerText(" ")
-bannerText("When you're feeling in the dumps,")
-bannerText("Don't be silly chumps,")
-bannerText("Just purse your lips and whistle - that's the thing!")
-bannerText("And... always look on the bright side of life...")
-bannerText("*")
+bannerText("*", 60)
+bannerText("Always look on the bright side of life...", 60)
+bannerText("If life seems jolly rotten", 60)
+bannerText("There's something you've forgotten!", 60)
+bannerText("And that's to laugh and smile and dance and sing,", 60)
+bannerText(" ", 60)
+bannerText("When you're feeling in the dumps,", 60)
+bannerText("Don't be silly chumps,", 60)
+bannerText("Just purse your lips and whistle - that's the thing!", 60)
+bannerText("And... always look on the bright side of life...", 60)
+bannerText("*", 60)
