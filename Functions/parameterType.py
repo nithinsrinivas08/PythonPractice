6c24bf2a828cca7f14d6203c6cbc4524b1ddc2bc
nithinsrinivas08 <86115904+nithinsrinivas08@users.noreply.github.com>
2023-07-08 15:47:59 +0200

    Update parameterType.py
    clear error in parameter file

--- Functions/parameterType.py
+++ Functions/parameterType.py
@@ -4,4 +4,4 @@ def func(p1, p2, *args, k, **kwargs) :
     print("keyword:...........{}".format(k))
     print("var-keyword:..........{}".format(kwargs))
 
-func(1,2,3,4,5,9,k=6, key1=7, key2 =8)
+func(1,2,3,4,5,9 ,k=6, key1=7, key2 =8)
