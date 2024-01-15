47dee0d637c47a343a705814776f95fe5c2c77b6
nithinsrinivas08 <86115904+nithinsrinivas08@users.noreply.github.com>
2024-01-15 23:57:48 +0100

    new function file

---
+++ Functions/functions.py
@@ -0,0 +1,15 @@
+def fibonacci(n: int) -> int:
+    """Return the nth fibonacci number, for positive n."""
+    if 0 <= n <= 1:
+        return n
+    n_minus1, n_minus2 = 1, 0
+    result = None
+    for f in range(n - 1):
+        result = n_minus2 + n_minus1
+        n_minus2 = n_minus1
+        n_minus1 = result
+    return result
+
+
+for i in range(20):
+    print(i, fibonacci(i))
