7b248606c20d2b0226a2e1ed256095b8d812a9a7
nithinsrinivas08 <86115904+nithinsrinivas08@users.noreply.github.com>
2024-01-15 22:28:08 +0100

    new practice file

--- Functions/fibonacciNumbers.py
+++ Functions/fibonacciNumbers.py
@@ -1,15 +1,16 @@
-def fibonacci(n) :
+def fibonacci(n):
     """Return the `n`th fibonacci number, for positive `n`."""
-    if 0 <= n <= 1 :
+    if 0 <= n <= 1:
         return n
-    nMinus1, nMinus2 = 1,0
+    n_minus1, n_minus2 = 1, 0
     result = None
-    for f in range(n-1):
-        result = nMinus2 + nMinus1
-        nMinus2 = nMinus1
-        nMinus1 = result
-    
+    for f in range(n - 1):
+        result = n_minus2 + n_minus1
+        n_minus2 = n_minus1
+        n_minus1 = result
+
     return result
+
 
 for i in range(36):
     print(i, fibonacci(i))
--- Functions/guessingGame.py
+++ Functions/guessingGame.py
@@ -1,34 +1,36 @@
 import random
 
-def getInteger(prompt) :
+
+def get_integer(prompt):
     """
     Get an integer from standard Inout (stdin)
-    The funciton will continue looping, and prompting.
+    The function will continue looping, and prompting.
     the user, until a valid `int` is entered.
-    :param prompt : The string that the user will see, whem they're prompted to enter the value 
+    :param prompt : The string that the user will see, when they're prompted to enter the value
     :return : The integer that the user enters
     """
     while True:
         temp = input(prompt)
         if temp.isnumeric():
             return int(temp)
-        #else :
+        # else :
         print("{} is not a valid number".format(temp))
- 
+
+
 print(input.__doc__)
-print ("*" * 80)
-print(getInteger.__doc__)
-print ("*" * 80)
+print("*" * 80)
+print(get_integer.__doc__)
+print("*" * 80)
 
 highest = 1000
 answer = random.randint(1, highest)
-#print(answer)  # TODO: Remove after testing.
+# print(answer)  # TODO: Remove after testing.
 guess = 0  # initialise to any number that doesn't equal answer
 print("Please guess a number between 1 and {}: ".format(highest))
- 
+
 while guess != answer:
-    guess = getInteger(": ")
- 
+    guess = get_integer(": ")
+
     if guess == 0:
         break
     if guess == answer:
